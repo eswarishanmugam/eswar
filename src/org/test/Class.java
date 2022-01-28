4338b0b7e87a8c558ec8da36145f9bd04e42901b
eswarishanmugam <esawrist11@gmail.com>
2022-01-28 12:30:43 +0530

    done

--- src/org/test/Class.java
+++ src/org/test/Class.java
@@ -1,6 +1,9 @@
 package org.test;
 
 public class Class {
+	private void m2() {
+	System.out.println("everything done");
+	}	
 public static void main(String[] args) {
 	System.out.println("done");
 }
